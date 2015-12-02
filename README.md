b2b287a481c365d9ee7baa54fb149925fbae043c
coopermj <micah.cooper@gmail.com>
2015-12-02 11:29:13 -0500

    Initial commit
    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # marked-graphviz
+
+This is a pre-processor for Marked2.app that
+
+1. Reads in the Markdown
+2. Finds <dot> and <neato> blocks
+3. Strips them and runs them through dot and neato on the command line, creating files named whatever follows graph/digraph
+4. Returns the markdown through stdout with the dot and neato blocks replaced with references to inline images
+
---
+++ graphviz-pp.rb
@@ -0,0 +1,137 @@
+#!/usr/bin/env ruby
+# Pre-processor for Marked2.app
+# Takes markdown and spits out any graphviz diagrams into
+# tagged files and rewrites markdown to reference those files
+# Created by Micah Cooper based on the work of Brett Terpstra
+
+require 'rubygems'
+require 'nokogiri'
+require 'open3'
+include Open3
+
+customStyle = 1
+
+# set our current directory to the doc's directory
+if (ENV['MARKED_ORIGIN'])
+	Dir.chdir(ENV['MARKED_ORIGIN'])
+end
+
+def customStyler(name)
+		# Open the svg we just created
+		svg = File.open(name) { |f| Nokogiri::XML(f) }
+		# Strip out the default font-family and size
+		# I couldn't find a way to suppress the creation and we need to open the file for
+		# modification anyway
+		svg.xpath('//@font-family').remove
+		svg.xpath('//@font-size').remove
+
+		# Get our custom css file – in the future I hope we can just bring in the current Marked css file!
+		css = File.open('yourfileherewithcompletepath') { |f| Nokogiri::HTML(f) }
+
+		# Create our defs/style structure and layer it in
+		defs_node = Nokogiri::XML::Node.new("defs", svg)
+		style_node = Nokogiri::XML::Node.new("style", svg)
+		style_node['type'] = "text/css"
+
+		cdata = "![CDATA[\n" << css.inner_text << "\n]]"
+		style_node.add_child(cdata)
+		#cdata_node = Nokogiri::XML::children.new(cdata, svg)
+
+		#style_node << cdata_node
+		defs_node << style_node
+		svg.root << defs_node  
+		#span_node = Nokogiri::XML::Node.new('span',svg)
+		#span_node['class'] = 'defs'
+		#svg.children.first.add_previous_sibling(span_node)
+
+		# Save our changes
+		File.write(name, svg.to_xml)
+end
+
+# Get content from STDIN
+content = STDIN.read
+
+# capture content into parsing system
+html_doc = Nokogiri::HTML(content)
+
+
+#find all the <dot> references
+#puts html_doc.xpath("//dot")
+allDots = html_doc.xpath("//dot")
+allDots.each do |dot|
+	dotString = dot.text
+	# use whatever is before the { for naming
+	basename = dotString.match(/(.*){/).to_s
+	#puts basename
+	caption = dotString.match(/graph (.*)(\s*){/)[1]
+	caption = caption.rstrip
+	caption.gsub!(/_/, ' ')
+	#....need to 
+	#caption = fullcaption[1]
+
+
+	# remove all the whitespace and non-characters
+	name = basename.gsub!(/\W+/, '') << ".svg"
+
+	# set our options and command line
+	runstring = "dot -Tsvg -o #{name}"
+
+	# shell out to the command line and run the above command
+	output = Open3.capture2(runstring, :stdin_data=>dotString)
+
+	# Add a new entry in markdown to reference the file we just created
+	dot.add_next_sibling("![#{caption}](#{name})")
+
+	# remove the <dot> entry from the html
+	dot.remove
+	
+	# customStyle lets us pull in a specific css file to append to our svg
+	# since passing along styling through graphviz can be a pain (and non-modern)
+	if (1 == customStyle)
+		customStyler(name)
+	end
+end
+
+#find all the <neato> references
+allNeats = html_doc.xpath("//neato")
+allNeats.each do |neat|
+	neatString = neat.text
+	# use whatever is before the { for naming
+	basename = neatString.match(/(.*){/).to_s
+	#puts basename
+	caption = neatString.match(/graph (.*)(\s*){/)[1]
+	caption = caption.rstrip
+	caption.gsub!(/_/, ' ')
+	#....need to 
+	#caption = fullcaption[1]
+
+
+	# remove all the whitespace and non-characters
+	name = basename.gsub!(/\W+/, '') << ".svg"
+
+	# set our options and command line
+	runstring = "neato -Tsvg -o #{name}"
+
+	# shell out to the command line and run the above command
+	output = Open3.capture2(runstring, :stdin_data=>neatString)
+
+	# Add a new entry in markdown to reference the file we just created
+	neat.add_next_sibling("![#{caption}](#{name})")
+
+	# remove the <neato> entry from the html
+	neat.remove
+	
+	# customStyle lets us pull in a specific css file to append to our svg
+	# since passing along styling through graphviz can be a pain (and non-modern)
+	if (1 == customStyle)
+		customStyler(name)
+	end
+end
+
+
+
+
+# return just the text (not html) to STDOUT for Marked to process
+puts html_doc.inner_text
+
+
